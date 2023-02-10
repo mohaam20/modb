c618b1e75418b2cf0888104fca987a691eab54d3
mohaam20 <76072798+mohaam20@users.noreply.github.com>
2023-02-10 09:02:30 +0200

    Add files via upload

--- scripts/details.js
+++ scripts/details.js
@@ -5,7 +5,7 @@
 // if (location.hash.length == 0) {
 //   removeEventListener("popstate", channnn);
 // }
-// console.log = function () {};
+console.log = function () {};
 
 addEventListener("popstate", channnn);
 
